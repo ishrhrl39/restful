59be737979f02106fc3d6980bf0aae3f6b5e1787
이현섭 <@DESKTOP-4VPH1H9>
2019-04-06 15:12:34 +0900

    test

--- src/com/mnwise/carrym/wiseu/rest/util/CarrymController.java
+++ src/com/mnwise/carrym/wiseu/rest/util/CarrymController.java
@@ -1,7 +1,32 @@
 package com.mnwise.carrym.wiseu.rest.util;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CarrymController {
 	public CarrymController() {
 		
+	}
+	
+	
+	
+	private static Map checkVo(Object object, String ... columnList) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		Map returnData = new HashMap();
+		for(String columnNm : columnList) {
+			Field field = object.getClass().getDeclaredField(columnNm);
+			field.setAccessible(true);
+			String value = (String)field.get(object);
+			if(value == null || value.equals("")) {
+				returnData.put("result", false);
+				returnData.put("columnNm", columnNm);
+				return returnData;
+			}else {
+				continue;
+			}
+		}
+		
+		returnData.put("result", true);
+		return returnData;
 	}
 }
