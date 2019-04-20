4b269f1755bf015de00598fbe0960706b24cac6c
이현섭 <@DESKTOP-4VPH1H9>
2019-04-20 15:35:48 +0900

    에러코드 502, 503 추가

--- src/com/mnwise/carrym/wiseu/rest/util/Constants.java
+++ src/com/mnwise/carrym/wiseu/rest/util/Constants.java
@@ -12,5 +12,7 @@ public class Constants {
 		public static final String NO_USER = "600";	// 유저없음
 		public static final String NO_VALUE = "500"; // 컬럼값 없음
 		public static final String DUPL_SEQ = "501"; // 이미 존재하는 SEQ
+		public static final String NOFILE_DB = "502"; // 파일이 DB내 존재하지않음
+		public static final String NOFILE_PATH = "503"; // 파일이 경로내 존재하지않음
 	}
 }
